d8f62285b450cf8a9406b03fa84f20263500740e
Lsai7 <lsaisundar7@gmail.com>
2022-02-05 12:37:09 +0530

    Welcome Friends...

---
+++ src/main/java/com/SDET_26/Friends/App.java
@@ -0,0 +1,13 @@
+package com.SDET_26.Friends;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/SDET_26/Friends/WelcomeFreinds.java
@@ -0,0 +1,13 @@
+package com.SDET_26.Friends;
+
+public class WelcomeFreinds 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi All...");
+		System.out.println("Welcome To Friends Repository..");
+
+	}
+
+}
